4c5e18b1db5bb4554f79d3e66b652704b9f0fcd8
christopherekfeldt <christopher.ekfeldt@hotmail.se>
2018-04-19 13:36:25 +0200

    gitignore test

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import { Card, CardImg, CardText, CardBody,
+  CardTitle, CardSubtitle, Button } from 'reactstrap';
+
+/*   <div class="card">
+      <Card>
+        <CardImg width="40%" src="https://www.pexels.com/photo/alternative-alternative-energy-blue-eco-411592/" alt="Card image cap" />
+        <CardBody>
+          <CardTitle>Card title</CardTitle>
+          <CardSubtitle>Card subtitle</CardSubtitle>
+          <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+          <Button>Button</Button>
+        </CardBody>
+      </Card>
+    </div> */
 
 const LandingPage = () =>
-  <div>
-    <center>
-      <p>Welcome to Seeding Solar</p>
-    </center>
+    <div>
+      <div class="jumbotron">
+        <center>
+          <h1>Seeding Solar</h1> 
+          <p>Here we will do godlike work together</p>
+        </center>  
+      </div>
   </div>
 
 export default LandingPage;
